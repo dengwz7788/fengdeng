c5cd4f5ad5a0ea0495bf0222e4c39045e42535f2
01401892 <summer12@sfmail.sf-express.com>
2021-01-25 14:10:40 +0800

    提交的client

---
+++ client/src/page/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <van-notice-bar
+      left-icon="volume-o"
+      text="在代码阅读过程中人们说脏话的频率是衡量代码质量的唯一标准。"
+    />
+
+    <van-collapse v-model="activeNames">
+      <van-collapse-item title="用户信息" name="1">
+        <van-cell-group>
+          <van-cell title="用户ID" value="12345678" />
+          <van-cell title="用户名" value="苏默人" />
+          <van-cell title="职称"   value="商业" />
+        </van-cell-group>
+      </van-collapse-item>
+    </van-collapse>
+    <van-cell title="数据日期" :value="date" @click="show = true" />
+    <van-cell-group title="业绩数据">
+      <van-cell title="职称" v-if="empty"  value="商业" />
+      <van-empty description="暂时没有找到当天的数据" v-else />
+    </van-cell-group>
+
+    <van-calendar v-model="show" :min-date="minDate" :max-date="maxDate" @confirm="onConfirm" position="top"  />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      activeNames: ['1'],
+      date: '2020-01-25',
+      show: false,
+      empty: false,
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2021, 0, 31)
+    }
+  },
+  methods: {
+    formatDate (date) {
+      return `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`
+    },
+    onConfirm (date) {
+      this.show = false
+      this.date = this.formatDate(date)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.hello{
+   height: 100px;
+}
+</style>
